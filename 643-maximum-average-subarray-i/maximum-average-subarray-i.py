06266fb46d585eac1395a15260e8288d7e219e64
Karan Gupta <officialkarangupta06@gmail.com>
2026-09-26 19:07:27 +0530

    Time: 73 ms (33.82%) | Memory: 29.4 MB (18.49%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,10 +3,10 @@ class Solution:
         window_sum = 0
         for i in range(k):
             window_sum += nums[i]
-        max_sum= window_sum
+        max_sum = window_sum
 
         for i in range(k, len(nums)):
             window_sum += nums[i]-nums[i-k]
             max_sum = max(max_sum, window_sum)
-            
+        
         return max_sum/k
